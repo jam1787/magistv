4dc6d1d4fd6fa6060c6ace69b05154f5265d32b0
Joaquín Elía <joaquin.elia@hotmail.com>
2024-06-12 23:35:15 -0300

    Add Hero, Navbar and pricing components

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import heroImg from '../../../public/hero-magistv.png'
 import Image from 'next/image'
 
@@ -18,7 +17,7 @@ const Hero = async () => {
   const {title, description} = strapiData.data.attributes
 
   return (
-    <section className='2xl:container 2xl:m-auto relative min-h-screen flex justify-around md:justify-between items-center flex-col md:flex-row p-5 lg:px-8 text-white text-balance text-center md:text-start'>
+    <section className='2xl:container 2xl:m-auto relative h-screen max-h-[70rem] flex justify-around md:justify-between items-center flex-col md:flex-row p-5 lg:px-8 text-white text-balance text-center md:text-start'>
       <header className="mt-10 md:mt-0 max-w-xl">
         <h1 className="text-3xl font-medium leading-tight md:text-5xl md:leading-tight md:font-semibold lg:text-6xl lg:leading-[1.15]">
           {title}
@@ -26,9 +25,9 @@ const Hero = async () => {
         <p className='opacity-90 text-sm leading-relaxed font-light py-4 pb-6 md:py-6 md:pb-10 md:text-base md:w-90 md:mx-auto lg:text-lg'>
           {description}
         </p>
-        <Link href="#" className='px-7 py-3 font-medium bg-[#e7ebff] text-[#2245ff] rounded-full'>Prueba gratuita</Link>
+        <a href="#pricing" className='px-7 py-3 font-medium bg-[#e7ebff] text-[#2245ff] rounded-full'>Contratá ahora</a>
       </header>
-      <footer className='2xl:container md:absolute md:right-8 md:-z-10 w-full md:w-[50vw] md:max-w-[55rem] h-[15rem] md:h-[30rem] mt-0 md:mt-40 lg:mt-20 object-cover'>
+      <footer className='2xl:container md:absolute md:right-8 md:-z-10 w-full md:w-[50vw] md:max-w-[55rem] h-[15rem] md:h-[30rem] sm:mb-20 md:mt-40 lg:mt-20 object-cover'>
         <Image src={heroImg} alt='Imagen de peliculas, series y canales de MagisTV' />
       </footer>
     </section>
