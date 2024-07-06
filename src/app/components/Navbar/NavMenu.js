407e9ab9e095d9ded21de7c9504b1a8feb6585cd
Joaquín Elía <joaquin.elia@hotmail.com>
2024-07-06 03:11:04 -0300

    Add menu close to login link

--- src/app/components/Navbar/NavMenu.js
+++ src/app/components/Navbar/NavMenu.js
@@ -14,7 +14,7 @@ const NavMenu = ({
   return (
     <nav className="flex items-center justify-between text-white 2xl:container 2xl:m-auto px-5 lg:px-8 py-2 md:py-4">
         <Link href="/" className='w-16'>
-            <img src='/magis-tv.png' alt='Logo MagisTV'></img>
+            <img src='/magis-tv.png' alt='Logo MagisTV' />
         </Link>
         <ul className={`${menuClick ? menuStyles + menuActiveStyles : menuStyles}`}>
             {navLinks.map(({name, route}, i) => 
@@ -34,7 +34,7 @@ const NavMenu = ({
                     classStyle='py-2 px-5'
                 />
             </li>
-            <li>
+            <li onClick={()=> setMenuClick(false)}>
                 <Link href='sign-in'>
                     <HiOutlineUser className="text-3xl p-0" />
                 </Link>
--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = () => {
   const handleClick = () => setMenuClick(!menuClick)
 
   const menuStyles = 'flex absolute -z-10 top-[-100vh] left-0 gap-12 flex-col justify-center items-center text-xl h-screen w-full font-semibold md:font-normal transition-all duration-300 md:flex-row md:static md:gap-6 md:text-base md:h-0 md:w-auto md:transition-none md:z-0'
-  const menuActiveStyles = 'backdrop-blur-md bg-gradient-to-b from-[#0024d6] 80% to-[#010130ef] 100% top-[0] left-0'
+  const menuActiveStyles = 'backdrop-blur-sm bg-gradient-to-b from-[#0024d6] 80% to-[#010130ef] 100% top-[0] left-0'
 
   return (
-    <header className='fixed top-0 left-0 z-20 w-full backdrop-blur-md'>
+    <header className='fixed top-0 left-0 z-20 w-full backdrop-blur-sm'>
       <NavMenu
         menuStyles={menuStyles}
         menuActiveStyles={menuActiveStyles}
