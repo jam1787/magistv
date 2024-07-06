bb32ebf3137bd67b50ccfdc34701f6703a5d5c15
Joaquín Elía <joaquin.elia@hotmail.com>
2024-07-06 03:08:01 -0300

    Add new faq

--- src/app/components/Accordion/Accordion.js
+++ src/app/components/Accordion/Accordion.js
@@ -1,42 +1,53 @@
 "use client"
-import {useState} from 'react'
+import { useState } from 'react'
 import { AccordionAnimation } from './AccordionAnimation'
+import Link from 'next/link'
 
-export const Accordion = ({question, answer, allowDevices, devices}) => {
+export const Accordion = ({ question, answer, allowDevices, devices, downloadLink }) => {
     const [accordionOpen, setAccordionOpen] = useState(false)
 
-  return (
-    <div className='px-3 pt-5 pb-3 border-b cursor-pointer' onClick={() => setAccordionOpen(!accordionOpen)}>
-        <button
-            className='w-full text-start flex items-center justify-between gap-2'
-        >
-            <span className='text-pretty'>{question}</span>
-            <AccordionAnimation accordionOpen={accordionOpen}/>
-        </button>
-        <div className={`text-sm transition-all duration-300 py-1 h-0 overflow-hidden ${
-            accordionOpen && 'h-fit py-2'
-        }`}>
-            <div className="pt-2 transition-all">
-                <p className='opacity-85 w-[90%] text-ballance'>{answer}</p>
-                {devices && 
-                    allowDevices.map(({tv, stream, mobile}, i) => 
-                        <ul key={i} className='pt-4'>
-                            <h3 className='my-2 text-base'>Smart TVs</h3>
-                            {tv.map((tvs, i) => 
-                                <li key={i} className='pt-1.5 pl-3 opacity-85'>{tvs}</li>
-                            )}
-                            <h3 className='my-2 text-base'>Dispositivos de streaming</h3>
-                            {stream.map((streams, i) =>
-                                <li key={i} className='pt-1.5 pl-3 opacity-85'>{streams}</li>
-                            )}
-                            <h3 className='my-2 text-base'>Dispositivos móviles</h3>
-                            {mobile.map((mobiles, i) =>
-                                <li key={i} className='pt-1.5 pl-3 opacity-85'>{mobiles}</li>
-                            )}
-                        </ul>
-                    )}
+    return (
+        <div className='px-3 pt-5 pb-3 border-b cursor-pointer' onClick={() => setAccordionOpen(!accordionOpen)}>
+            <button
+                className='w-full text-start flex items-center justify-between gap-2'
+            >
+                <span className='text-pretty'>{question}</span>
+                <AccordionAnimation accordionOpen={accordionOpen} />
+            </button>
+            <div className={`text-sm transition-all duration-300 py-1 h-0 overflow-hidden ${accordionOpen && 'h-fit py-2'
+                }`}>
+                <div className="pt-2 transition-all">
+                    <p className='opacity-85 w-[90%] text-ballance'>{answer}</p>
+                    {devices &&
+                        allowDevices.map(({ tv, stream, mobile }, i) =>
+                            <ul key={i} className='pt-4'>
+                                <h3 className='my-2 text-base'>Smart TVs</h3>
+                                {tv.map((tvs, i) =>
+                                    <li key={i} className='pt-1.5 pl-3 opacity-85'>{tvs}</li>
+                                )}
+                                <h3 className='my-2 text-base'>Dispositivos de streaming</h3>
+                                {stream.map((streams, i) =>
+                                    <li key={i} className='pt-1.5 pl-3 opacity-85'>{streams}</li>
+                                )}
+                                <h3 className='my-2 text-base'>Dispositivos móviles</h3>
+                                {mobile.map((mobiles, i) =>
+                                    <li key={i} className='pt-1.5 pl-3 opacity-85'>{mobiles}</li>
+                                )}
+                            </ul>
+                        )
+                    }
+                    {downloadLink &&
+                        <Link 
+                            target='_blank'
+                            rel='no-referrer'
+                            className='block mt-3 underline'
+                            href={downloadLink}
+                        >
+                            Descargar MagisTv
+                        </Link>
+                    }
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/app/components/Faqs.js
+++ src/app/components/Faqs.js
@@ -4,7 +4,8 @@ const faqsData = [
     {question: '¿Qué se puede ver en MagisTv?', answer: 'Se trata de una aplicación que ofrece acceso a más de 1.200 canales en vivo, 10.000 series y películas, eventos deportivos y más. Además, permite el acceso ilimitado a todas las plataformas, entre esas Star+, Hbo, Disney, por lo que los usuarios pueden ver los partidos de LigaPro y Muchos más sin ningún coste adicional.'},
     {question: '¿Qué tan seguro es la aplicación MagisTV?', answer: 'Siempre y cuando lo descarguen de los Links proporcionados por nosotros no representa ningún riesgo. Como todo en internet, tratando de replicar el éxito de Magis han salido copias maliciosas que introducen Publicidad en Tu teléfono abusando de los Permisos que le Puedan otorgar.'},
     {question: '¿Cuales son los dispositivos compatibles?', answer: 'Basicamente todo Dispositivos Android donde puedas instalar la aplicacion de Magis es compatible.', devices: true },
-    {question: '¿Cuántos dispositivos puedo conectar al mismo tiempo?', answer: 'Podrás visualizar hasta 3 pantallas consecutivas nuestro servicio se vende en cuentas triples o para 3 dispositivos.'},
+    {question: '¿Cuántos dispositivos puedo conectar al mismo tiempo?', answer: 'Podrás visualizar hasta 3 pantallas consecutivas, nuestro servicio se vende en cuentas triples o para 3 dispositivos.'},
+    {question: '¿Dónde se puede descargar la aplicación?', answer: 'Puedes descargar la aplicación desde el siguiente link:', link: 'http://descargadeprueba.com'}
 ]
 
 const allowDevices = [{
@@ -20,13 +21,14 @@ export const Faqs = () => {
           <h2 className="text-3xl font-semibold leading-tight sm:text-4xl lg:text-5xl">Preguntas frecuentes</h2>
         </div>
         <div className="p-2 w-[90%] mx-auto max-w-[50rem] rounded-lg">
-          {faqsData.map(({question, answer, devices}, i) =>
+          {faqsData.map(({question, answer, devices, link}, i) =>
             <Accordion
               key={i}
               question={question}
               answer={answer}
               allowDevices={allowDevices}
               devices={devices}
+              downloadLink={link}
             />
           )}
         </div> 
