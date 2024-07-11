839567fb14b11c49a980e8f45c7456cbdbc3cbea
Joaquín Elía <joaquin.elia@hotmail.com>
2024-07-11 02:47:02 -0300

    Add new StrapiError message

--- src/app/(auth)/confirmation/ConfirmaitonError.js
+++ src/app/(auth)/confirmation/ConfirmaitonError.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const ConfirmationError = () => {
     return (
-      <p className='text-balance text-red-500 text-sm mt-3'>
+      <p className='text-sm'>
         Parece que todavía no has confirmado tu correo electronico. Verifica en 
         tu correo si tienes un correo de confirmación. ¿No lo encuentras?{' '}
         <Link href='/confirmation/newrequest' className='underline'>
--- src/app/components/Form/StrapiErrors.js
+++ src/app/components/Form/StrapiErrors.js
@@ -1,12 +1,27 @@
 import { ConfirmationError } from "@/app/(auth)/confirmation/ConfirmaitonError";
 
+const ErrorWrapper = ({ children }) =>
+  <div className="text-red-500 text-md mt-3 text-center text-balance">
+    {children}
+  </div>
+
 export function StrapiErrors({ error }) {
   if (!error?.message) return null;
   console.log(error)
-  return <div className="text-red-500 text-md mb-4 text-center text-pretty">
-    {error.message === 'Your account email is not confirmed' 
-      ? <ConfirmationError /> 
-      : error.message
-    }
-  </div>
+
+  if (error.message === 'Your account email is not confirmed')
+    return (
+      <ErrorWrapper>
+        <ConfirmationError />
+      </ErrorWrapper>
+    )
+
+  if (error.message === 'Invalid identifier or password')
+    return (
+      <ErrorWrapper>
+        El identificador o la contraseña son inválidos
+      </ErrorWrapper>
+    )
+
+  return <ErrorWrapper>{error.message}</ErrorWrapper>
 }
