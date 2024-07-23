7cbc34cdea466c24fdeb89af76bffca6c702e7da
Joaquín Elía <joaquin.elia@hotmail.com>
2024-07-22 22:23:56 -0300

    Update get-hero-data with revalidate

--- src/app/data/services/get-hero-data.js
+++ src/app/data/services/get-hero-data.js
@@ -1,7 +1,7 @@
 export const getStrapiData = async (path) => {
     const baseUrl = process.env.STRAPI_URL
     try {
-        const res = await fetch(baseUrl + path)
+        const res = await fetch(baseUrl + path, { next: { revalidate: 0 }, })
         const data = await res.json()
         return data.data.attributes
     } catch (error) {
